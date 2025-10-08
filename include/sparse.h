323cfa618aea40e2a5f928f4747f1a824bc7102a
Pierre Kraemer <kraemer@unistra.fr>
2025-10-08 14:24:34 +0200

    name std types + update the interface of some functions

--- include/sparse.h
+++ include/sparse.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define SCALAR double
-#define INDEX unsigned int
+#define INDEX int
 
 #ifdef __cplusplus
 extern "C"
@@ -9,13 +9,13 @@ extern "C"
 #endif /* __cplusplus */
 
     void mulSparseMatrixScalar(const void *mat, SCALAR scalar, void *matOut);
-    void subSparseMatrices(const void *matA, const void *matB, void *matOut);
+    void addSparseMatrices(const void *matA, const void *matB, void *matOut);
 
     void *createSparseMatrix(INDEX rows, INDEX cols);
     void *createSparseMatrixFromTriplets(INDEX rows, INDEX cols,
-                                         INDEX nb_triplets,
-                                         const INDEX *triplet_rows, const INDEX *triplet_cols, const SCALAR *triplet_vals);
-    void *createDiagonalSparseMatrixFromArray(INDEX size, const SCALAR *diag_vals);
+                                         const void *triplets,
+                                         INDEX nb_triplets);
+    void *createDiagonalSparseMatrixFromArray(const SCALAR *diag_vals, INDEX size);
     void solveSymmetricSparseLinearSystem(const void *mat, const SCALAR *b, SCALAR *x, INDEX size);
     void destroySparseMatrix(void *mat);
 
--- src/sparse.cpp
+++ src/sparse.cpp
@@ -1,54 +1,59 @@
 #include "sparse.h"
 #include <Eigen/Sparse>
 
+using SparseMatrix = Eigen::SparseMatrix<SCALAR, Eigen::ColMajor, INDEX>;
+using Vector = Eigen::Matrix<SCALAR, Eigen::Dynamic, 1>;
+using Triplet = Eigen::Triplet<SCALAR, INDEX>;
+
 extern "C"
 {
     void mulSparseMatrixScalar(const void *mat, SCALAR scalar, void *matOut)
     {
-        const Eigen::SparseMatrix<SCALAR> *sparseMat = static_cast<const Eigen::SparseMatrix<SCALAR> *>(mat);
-        Eigen::SparseMatrix<SCALAR> *sparseMatOut = static_cast<Eigen::SparseMatrix<SCALAR> *>(matOut);
+        const SparseMatrix *sparseMat = static_cast<const SparseMatrix *>(mat);
+        SparseMatrix *sparseMatOut = static_cast<SparseMatrix *>(matOut);
         *sparseMatOut = (*sparseMat) * scalar;
     }
 
-    void subSparseMatrices(const void *matA, const void *matB, void *matOut)
+    void addSparseMatrices(const void *matA, const void *matB, void *matOut)
     {
-        const Eigen::SparseMatrix<SCALAR> *sparseMatA = static_cast<const Eigen::SparseMatrix<SCALAR> *>(matA);
-        const Eigen::SparseMatrix<SCALAR> *sparseMatB = static_cast<const Eigen::SparseMatrix<SCALAR> *>(matB);
-        Eigen::SparseMatrix<SCALAR> *sparseMatOut = static_cast<Eigen::SparseMatrix<SCALAR> *>(matOut);
-        *sparseMatOut = (*sparseMatA) - (*sparseMatB);
+        const SparseMatrix *sparseMatA = static_cast<const SparseMatrix *>(matA);
+        const SparseMatrix *sparseMatB = static_cast<const SparseMatrix *>(matB);
+        SparseMatrix *sparseMatOut = static_cast<SparseMatrix *>(matOut);
+        *sparseMatOut = (*sparseMatA) + (*sparseMatB);
     }
 
     void *createSparseMatrix(INDEX rows, INDEX cols)
     {
-        return new Eigen::SparseMatrix<SCALAR>(rows, cols);
+        return new SparseMatrix(rows, cols);
     }
 
     void *createSparseMatrixFromTriplets(INDEX rows, INDEX cols,
-                                         INDEX nb_triplets,
-                                         const INDEX *triplet_rows, const INDEX *triplet_cols, const SCALAR *triplet_vals)
+                                         const void *triplets,
+                                         INDEX nb_triplets)
     {
-        Eigen::SparseMatrix<SCALAR> *mat = new Eigen::SparseMatrix<SCALAR>(rows, cols);
-        std::vector<Eigen::Triplet<SCALAR>> triplets;
-        triplets.reserve(nb_triplets);
-        for (size_t i = 0; i < nb_triplets; ++i)
-            triplets.emplace_back(triplet_rows[i], triplet_cols[i], triplet_vals[i]);
-        mat->setFromTriplets(triplets.begin(), triplets.end());
+        const Triplet *triplets_ptr = static_cast<const Triplet *>(triplets);
+        SparseMatrix *mat = new SparseMatrix(rows, cols);
+        std::vector<Triplet> triplets_vec;
+        triplets_vec.reserve(nb_triplets);
+        for (INDEX i = 0; i < nb_triplets; i++)
+            triplets_vec.push_back(triplets_ptr[i]);
+        mat->setFromTriplets(triplets_vec.begin(), triplets_vec.end());
         return mat;
     }
 
-    void *createDiagonalSparseMatrixFromArray(INDEX size, const SCALAR *diag_vals)
+    void *createDiagonalSparseMatrixFromArray(const SCALAR *diag_vals, INDEX size)
     {
-        Eigen::Map<const Eigen::VectorXd> diag(diag_vals, size);
-        return new Eigen::SparseMatrix<SCALAR>(diag.asDiagonal());
+        Eigen::Map<const Vector> diag(diag_vals, size);
+        return new SparseMatrix(diag.asDiagonal());
     }
 
     void solveSymmetricSparseLinearSystem(const void *mat, const SCALAR *b, SCALAR *x, INDEX size)
     {
-        const Eigen::SparseMatrix<SCALAR> *sparseMat = static_cast<const Eigen::SparseMatrix<SCALAR> *>(mat);
-        Eigen::Map<const Eigen::VectorXd> bVec(b, size);
-        Eigen::Map<Eigen::VectorXd> xVec(x, size);
+        const SparseMatrix *sparseMat = static_cast<const SparseMatrix *>(mat);
+        Eigen::Map<const Vector> bVec(b, size);
+        Eigen::Map<Vector> xVec(x, size);
 
-        Eigen::SimplicialLDLT<Eigen::SparseMatrix<SCALAR>> solver(*sparseMat);
+        Eigen::SimplicialLDLT<SparseMatrix> solver(*sparseMat);
         xVec = solver.solve(bVec);
     }
 
